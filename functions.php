82186860821f1fd0bf571c9b66a320ceb90b1b40
ImThomasThorne <me@thomasthorne.me>
2024-04-13 18:59:17 +0100

    Added Github Updater

--- functions.php
+++ functions.php
@@ -19,3 +19,19 @@ function youthalliance_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'youthalliance_styles' );
 
+// Github Updater
+
+require 'plugin-update-checker/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/ImThomasThorne/youthalliance',
+	__FILE__,
+	'youthalliance'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+//Optional: If you're using a private repository, specify the access token like this:
+$myUpdateChecker->setAuthentication('your-token-here');
